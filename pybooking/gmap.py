0624555681273d18b2126196354d90ad693cfe7c
Alice Wang <alice@forevercapital.com>
2017-07-25 11:33:13 +0200

    change coor to city name

--- pybooking/gmap.py
+++ pybooking/gmap.py
@@ -7,13 +7,24 @@ import googlemaps
 import util
 
 
+def _get_coordinates(info):
+    xy = info['geometry']['location']
+    return xy['lat'], xy["lng"]
+
+
 class PlaceClient(object):
     def __init__(self):
-        self.client = googlemaps.Client(util.APIKeys.place)
+        self.client_geo = googlemaps.Client(util.APIKeys.geocode)
+        self.client_place = googlemaps.Client(util.APIKeys.place)
         self.default_radius = 2000  # meters
 
-    def places_nearby(self, location, place_type):
-        result = self.client.places(
+    def get_location(self, city):
+        result = self.client_geo.geocode(city)
+        return _get_coordinates(result[0])
+
+    def places_nearby(self, city, place_type):
+        location = self.get_location(city)
+        result = self.client_place.places(
             place_type, location=location, radius=self.default_radius
         )
         all_places = sorted(
@@ -21,13 +32,13 @@ class PlaceClient(object):
         )
         filename = path.join(
             util.OUTPUT_DIR,
-            '{}_{}.json'.format('-'.join(location), place_type)
+            '{}_{}.json'.format(city, place_type)
         )
         json.dump(all_places, open(filename, "w"))
 
 
 if __name__ == "__main__":
-    # python pybooking/gmap.py 48.852737 2.350699 museum
+    # python pybooking/gmap.py Paris museum
     client = PlaceClient()
-    x, y, place_type = sys.argv[1:]
-    client.places_nearby((x, y), place_type)
+    city, place_type0 = sys.argv[1:]
+    client.places_nearby(city, place_type0)
